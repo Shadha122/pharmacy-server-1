c030bea46891f8055acc3357c9c5db126c1341a6
Shadha122 <16J21194@utas.edu.om>
2024-12-22 09:03:41 +0400

    Update index.js

--- index.js
+++ index.js
@@ -159,6 +159,12 @@ app.get('/orders/:userId', async (req, res) => {
   }
 });
 
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Pharmacy Server!');
+});
+
+
 // Start the Server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
